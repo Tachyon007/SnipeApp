2bab27e5a0a36ece846811fb8204708711d89bf9
Tachyon <kev.jelinek@gmail.com>
2024-12-24 22:54:57 -0500

    camera function

--- app/(tabs)/camera.tsx
+++ app/(tabs)/camera.tsx
@@ -3,16 +3,160 @@ import { StyleSheet, TouchableOpacity, Text, View, Button, Pressable } from 'rea
 import React, { useRef, useState, useEffect } from 'react';
 //import EditScreenInfo from '@/components/EditScreenInfo';
 //import Camera from '../../components/Camera';
-//import { CameraView, useCameraPermissions, PermissionStatus, PermissionResponse } from 'expo-camera';
+import { CameraView, useCameraPermissions, PermissionStatus, PermissionResponse } from 'expo-camera';
 //import { TabActions, TabRouter } from '@react-navigation/native';
-//import { Link } from 'expo-router';
-//import * as FileSystem from 'expo-file-system';
+import { Link } from 'expo-router';
+import * as FileSystem from 'expo-file-system';
 
 
 export default function TabTwoScreen() {
+
+  const [hasPermission, setHasPermission] = useState(false);
+  const cameraRef = useRef<CameraView | null>(null); // create a reference to the camera
+
+
+  const handleBase64Img = async (imgString: String) => {
+    if(imgString.length < 50){//More sanity checks?
+      console.log("b64 too short?");
+      return;
+    }
+
+    console.log("Img String Length: " + imgString.length);
+    console.log(imgString.substring(0, 100)); // Print the first 100 characters
+    
+    try { 
+
+      console.log("[Total b64 chars]: " + imgString.length);
+
+      //Send data to our endpoint
+      await fetch( 
+          'https://snipeapi.azurewebsites.net/api/ImageUpload_V2', 
+          {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+
+            //Set content of message
+            body: JSON.stringify({
+              base64: imgString,
+            })
+          })
+          //Proccess the response, check if it worked
+          .then(response => { 
+            response.text() 
+            .then(data => { 
+              //Check for success
+              console.log(data);
+            });
+          })
+  } 
+  catch (error) { 
+    //setSignUpResponse('There was an error');
+    console.log(error);
+  } 
+  }
+
+  const uploadByteArr = async (byteArr: any) => {
+    await fetch( 
+      'https://snipeapi.azurewebsites.net/api/ImageUpload_V3', 
+      {
+        method: 'POST',
+        headers: {
+         "Content-Type": "application/octet-stream",
+        },
+        body: byteArr,
+      })
+      .then(response => { 
+        response.text() 
+        .then(data => { 
+          console.log(data);
+        });
+      })
+  }
+  const handleCapture = async () => {
+    if (cameraRef.current) {
+      try {
+        //const photo = await cameraRef.current.takePictureAsync();
+        //console.log("photo taken", photo.base64);
+
+        cameraRef.current.takePictureAsync({quality: 0.1, imageType: "jpg"}).then(async (photo) => {
+          try {
+            if (photo == null) {
+              return;
+            }
+
+            const photoURI = photo.uri;
+
+            // Read the file as base64
+            const base64Data = await FileSystem.readAsStringAsync(photoURI, {
+              encoding: FileSystem.EncodingType.Base64
+            });
+
+            // If you still need binary data (as a Uint8Array), you can convert it from base64
+            const binaryImageData = atob(base64Data).split('').map(char => char.charCodeAt(0));
+            const byteArr = new Uint8Array(binaryImageData);
+
+            console.log("\n\n ----- New system test -----");
+            console.log("byte len: \t" + byteArr.length);
+            console.log("first 20: \t" + byteArr.toString().substring(0, 20));
+            uploadByteArr(byteArr);
+
+          } catch (error) {
+            console.error('Error uploading image:', error);
+          }
+        });
+        
+      } catch (error) {
+        console.log("error taking picture", error);
+      }
+    }
+  }
+ 
+const denyPermission = () => {
+    setHasPermission(false);
+  };
+ 
+  useEffect(() => {
+    (async () => {
+      const PermissionResponse = await useCameraPermissions();
+      if (PermissionStatus.GRANTED) {
+        setHasPermission(true);
+      } else {
+        setHasPermission(false);
+      }
+    })();
+  }, []);
+
+  if (!hasPermission) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center', color:'white' }}>We need your permission to show the camera
+        </Text>
+          <Button onPress={ () =>{setHasPermission(true)}} title="Yes" >
+        </Button>
+        <Pressable onPress={()=>{denyPermission}}>
+          <Text style={styles.captureButtonText}>No</Text>
+        </Pressable>
+      </View>
+    );
+  }
+else {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Snipe Camssss</Text>
+     
+    
+        <CameraView ref={cameraRef} style={styles.cameraContainer}>
+        <View>
+      
+      <TouchableOpacity onPress={()=>{handleCapture()}} style={styles.captureButton}>
+        <Text style={styles.captureButtonText}>Snipe</Text>
+      </TouchableOpacity>
+      </View>
+      </CameraView>
 
       <View style={styles.circlereticle}>
       <View style={styles.horizontalLine} />
@@ -20,7 +164,7 @@ export default function TabTwoScreen() {
       </View>
     </View>)
     
-}
+}}
 
 const styles = StyleSheet.create({
   container: {
